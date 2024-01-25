4de7a90c0b6cf73abbabb48eb2460e89861773ec
06ED <hsbest14@mail.ru>
2024-01-25 14:48:50 +0300

    Started creating menu api

--- lib/app/controllers/menu_controller.dart
+++ lib/app/controllers/menu_controller.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/app/controllers/controller.dart';
+import 'package:flutter_app/app/models/dish.dart';
+import 'package:nylo_framework/nylo_framework.dart';
+
+import '../networking/menu_api_service.dart';
 
 class MenuController extends Controller {
   @override
   construct(BuildContext context) {
     super.construct(context);
   }
+
+  Future<List<Dish>> getDishes() async =>
+      await api<MenuApiService>((request) => request.fetchMenu());
 }
